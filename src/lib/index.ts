d0296695cf8ea159fe1faa4f743ed788bfb19440
Mahdi Parivash <mahdiparivash@protonmail.com>
2024-04-05 19:58:11 +0330

    initial project

---
+++ src/lib/index.ts
@@ -0,0 +1,5 @@
+import { dev } from '$app/environment';
+
+export const title = 'Shakespeare';
+export const description = 'sveltekit blog for poets';
+export const url = dev ? 'htt[://localhost:5173' : 'url';
