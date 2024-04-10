a341c227d56f98879356a32ce00ea6b8ce61f7ba
Mahdi Parivash <mahdiparivash@protonmail.com>
2024-04-10 14:28:11 +0330

    feat:complete layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,6 +2,42 @@
 	import 'open-props/style';
 	import 'open-props/normalize';
 	import 'open-props/buttons';
+	import Header from '../components/header.svelte';
+	import Footer from '../components/footer.svelte';
+	import PageTransition from '../components/transation.svelte';
+
+	export let data;
+
+	import '../app.css';
 </script>
 
-<slot />
+<div class="layout">
+	<Header />
+	<main>
+		<PageTransition url={data.url}>
+			<slot />
+		</PageTransition>
+	</main>
+	<Footer />
+</div>
+
+<style>
+	.layout {
+		height: 100%;
+		max-inline-size: 1440px;
+		display: grid;
+		grid-template-rows: auto 1fr auto;
+		margin-inline: auto;
+		padding-inline: var(--size-7);
+	}
+
+	main {
+		padding-block: var(--size-9);
+	}
+
+	@media (min-width: 1440px) {
+		.layout {
+			padding-inline: 0;
+		}
+	}
+</style>
